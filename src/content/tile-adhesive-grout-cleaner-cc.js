b09c316ec4cdf28f16f802e4b2fb3ad2278a654e
Anurag Pal <apal895@gmail.com>
2023-09-21 00:29:52 +0530

    setup static routes using js

---
+++ src/content/tile-adhesive-grout-cleaner-cc.js
@@ -0,0 +1,64 @@
+const data = [
+  {
+    title: "JSW TYLFIX TYPE 1",
+    subTitle: "TILE ADISIVE FOR FLOOR & WALL- CERAMIC TILES",
+    slug: "jsw-tylfix-type-1",
+    description:
+      "JSW Tylfix Type 1 is a polymer modified cement based powder tile adhesive for fixing all type of ceramic, clay tiles, It is suitable for both vertical and horizontal application. It gives excellent bond on exterior cementitious surfaces like plaster or concrete etc.",
+    features: [
+      "Easy To Use",
+      "Water Resistant",
+      "Better Adhesive Bond Strength",
+    ],
+    benefits: [
+      "Easy to use - Just add water as per recommended W/P ratio on site and ready to use.",
+      "Can be used in internal dry areas, for floor as well as wall application.",
+      "Better adhesive bond strength & Bonds to various cement-based substrates.",
+      "Self-curing properties.",
+    ],
+    specificationsTable: [
+      {
+        property: "Tentative mixing ration (Powder: Water)",
+        result: "3.5 : 1 by volume;\n1 : 0.22 by weight",
+      },
+      {
+        property: "Mixed density",
+        result: "1.7 - 1.9 kg/ltr",
+      },
+      {
+        property: "Table flow",
+        result: "180 mm",
+      },
+      {
+        property: "Open time (IS 15477:2019)",
+        result: "Approx. 20 minutes",
+      },
+      {
+        property: "Adjustability\n(IS 15477:2019)",
+        result: "Approx. 20 minutes",
+      },
+      {
+        property: "Pot life",
+        result: "Approx. 60 minutes",
+      },
+      {
+        property: "Tensile adhesion strength\nas per (IS 15477:2019)",
+        result: "≥ 0.5 N/ mm",
+      },
+      {
+        property: "Application temperature\n(substrates & materials",
+        result: "5°C to 35°C",
+      },
+      {
+        property: "Adhesive thickness",
+        result: "3 - 12 mm",
+      },
+      {
+        property: "Settings/ trafficable time",
+        result: "24 hours",
+      },
+    ],
+  },
+];
+
+export default data;
