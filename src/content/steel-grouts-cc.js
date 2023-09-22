a0fce2aa713d17b80c95ca358feb4365766064e7
shishirtiwari23 <shishir456tiwari@gmail.com>
2023-09-22 21:22:54 +0530

    changes

---
+++ src/content/steel-grouts-cc.js
@@ -0,0 +1,220 @@
+const data = [
+  {
+    title: "SG 1 FLOWABLE",
+    subTitle: "HIGH PERFORMANCE & HIGH STRENGTH GROUT",
+    slug: "sg-1-flowable",
+    image: "/images/steel-grouts/SG-1-FLOWABLE.png",
+    category: "precision-steel-grouts",
+    description:
+      "JSW Steel Grout SG 1 is a cementitious flow-able non shrink high strength grout for precision application like repair RCC Structures heavy machinery foundations etc.",
+    features: ["Highly Flowable", "Non shrink Non Expansive", "High Strength"],
+    packages: ["25 Kg"],
+    benefits: [
+      "High early strength and highly flowable",
+      "Highly Durable",
+      "Non Expansive",
+      "Non Shrink",
+      "High Compressive, flexural & Tensile Strength",
+      "Early loading",
+      "Self-Compacting",
+      "High Bond Strength",
+    ],
+    relatedProducts: [
+      {
+        title: "JSW TYLFIX TYPE 2",
+        image: "/images/tile-adhesive/JSW TYLFIX TYPE 2.png",
+        link: "/tile-adhesive-grout-cleaner-cc/jsw-tylfix-type-2",
+      },
+      {
+        title: "JSW TYLFIX TYPE 3",
+        image: "/images/tile-adhesive/JSW TYLFIX TYPE 3.png",
+        link: "/tile-adhesive-grout-cleaner-cc/jsw-tylfix-type-2",
+      },
+      {
+        title: "JSW TYLFIX TYPE 4",
+        image: "/images/tile-adhesive/JSW TYLFIX TYPE 4.png",
+        link: "/tile-adhesive-grout-cleaner-cc/jsw-tylfix-type-4",
+      },
+    ],
+    specificationsTable: [
+      {
+        property: "Appearance",
+        result: "Free Flowing Powder",
+      },
+      {
+        property: "Color",
+        result: "Cementitious Grey",
+      },
+      {
+        property: "Water/ Powder Ratio",
+        result: "0.11 – 0.13",
+      },
+      {
+        property: "Flow on Table @ 30°C in mm",
+        result: "Min. 260 mm",
+      },
+      {
+        property: "Fresh Wet Density, Kg/m3",
+        result: "2200 – 2400",
+      },
+      {
+        property: "Initial Setting Time, Minutes",
+        result: "> 200",
+      },
+      {
+        property: "Final Setting Time, Minutes",
+        result: "< 600",
+      },
+      {
+        property: "Pot Life @ 30°C",
+        result: "45 – 60 minutes",
+      },
+    ],
+  },
+  {
+    title: "SG 2 FLOWABLE",
+    subTitle: "HIGH PERFORMANCE GROUT",
+    slug: "sg-2-flowable",
+    image: "/images/steel-grouts/SG-2-FLOWABLE.png",
+    category: "precision-steel-grouts",
+    description:
+      "JSW Steel Grout SG 2 is a cementitious highly flow-able non shrink high strength grout for precision application like repair RCC Structures, heavy machinery foundations etc.",
+    features: ["Non-shrink Grout", "Self-compactable", "High Durability"],
+    packages: ["25 kg"],
+    benefits: [
+      "High early strength and highly flowable",
+      "Highly Durable",
+      "Non Expansive",
+      "Non Shrink",
+      "High Compressive, flexural & Tensile Strength",
+      "Early loading",
+      "Self-Compacting",
+      "High Bond Strength",
+    ],
+    relatedProducts: [
+      {
+        title: "JSW TYLFIX TYPE 1",
+        image: "/images/tile-adhesive/JSW TYLFIX  TYPE 1.png",
+        link: "/tile-adhesive-grout-cleaner-cc/jsw-tylfix-type-1",
+      },
+      {
+        title: "JSW TYLFIX TYPE 3",
+        image: "/images/tile-adhesive/JSW TYLFIX TYPE 3.png",
+        link: "/tile-adhesive-grout-cleaner-cc/jsw-tylfix-type-3",
+      },
+      {
+        title: "JSW TYLFIX TYPE 4",
+        image: "/images/tile-adhesive/JSW TYLFIX TYPE 4.png",
+        link: "/tile-adhesive-grout-cleaner-cc/jsw-tylfix-type-4",
+      },
+    ],
+    specificationsTable: [
+      {
+        property: "Appearance",
+        result: "Free Flowing Powder",
+      },
+      {
+        property: "Color",
+        result: "Cementitious Grey",
+      },
+      {
+        property: "Water/ Powder Ratio",
+        result: "0.13 – 0.15",
+      },
+      {
+        property: "Flow on Table @ 30°C in mm",
+        result: "Min. 260 mm",
+      },
+      {
+        property: "Fresh Wet Density, Kg/m3",
+        result: "2200 – 2400",
+      },
+      {
+        property: "Initial Setting Time, Minutes",
+        result: "> 200",
+      },
+      {
+        property: "Final Setting Time, Minutes",
+        result: "< 600",
+      },
+      {
+        property: "Pot Life @ 30°C",
+        result: "45 – 60 minutes",
+      },
+    ],
+  },
+  {
+    title: "SG 3 FLOWABLE",
+    subTitle: "GENERAL PURPOSE PRECISION GROUT",
+    slug: "sg-3-flowable",
+    image: "/images/steel-grouts/SG-3-FLOWABLE.png",
+    category: "precision-steel-grouts",
+    description:
+      "JSW Steel Grout SG 3 is a cementitious highly flow-able non shrink high strength grout for precision application like repair RCC Structures, heavy machinery foundations etc.",
+    features: ["Non-shrink Grout", "Self-compactable", "High Durability"],
+    packages: ["25 kg"],
+    benefits: [
+      "High early strength and highly flowable",
+      "Highly Durable",
+      "Non Expansive",
+      "Non Shrink",
+      "High Compressive, flexural & Tensile Strength",
+      "Early loading",
+      "Self-Compacting",
+      "High Bond Strength",
+    ],
+    relatedProducts: [
+      {
+        title: "JSW TYLFIX TYPE 1",
+        image: "/images/tile-adhesive/JSW TYLFIX  TYPE 1.png",
+        link: "/tile-adhesive-grout-cleaner-cc/jsw-tylfix-type-1",
+      },
+      {
+        title: "JSW TYLFIX TYPE 3",
+        image: "/images/tile-adhesive/JSW TYLFIX TYPE 3.png",
+        link: "/tile-adhesive-grout-cleaner-cc/jsw-tylfix-type-3",
+      },
+      {
+        title: "JSW TYLFIX TYPE 4",
+        image: "/images/tile-adhesive/JSW TYLFIX TYPE 4.png",
+        link: "/tile-adhesive-grout-cleaner-cc/jsw-tylfix-type-4",
+      },
+    ],
+    specificationsTable: [
+      {
+        property: "Appearance",
+        result: "Free Flowing Powder",
+      },
+      {
+        property: "Color",
+        result: "Cementitious Grey",
+      },
+      {
+        property: "Water/ Powder Ratio",
+        result: "0.13 – 0.15",
+      },
+      {
+        property: "Flow on Table @ 30°C in mm",
+        result: "Min. 260 mm",
+      },
+      {
+        property: "Fresh Wet Density, Kg/m3",
+        result: "2200 – 2400",
+      },
+      {
+        property: "Initial Setting Time, Minutes",
+        result: "> 200",
+      },
+      {
+        property: "Final Setting Time, Minutes",
+        result: "< 600",
+      },
+      {
+        property: "Pot Life @ 30°C",
+        result: "45 – 60 minutes",
+      },
+    ],
+  },
+];
+
+export default data;
