6b82668086a9a18ff1e8f44c91659535fcf09048
Anurag Pal <apal895@gmail.com>
2023-09-22 19:36:09 +0530

    added content, created product page comp

---
+++ src/content/mortar-cc.js
@@ -0,0 +1,117 @@
+const data = [
+  {
+    title: "ENDURO PLAST",
+    subTitle: "READY MIX PLASTER",
+    slug: "enduro-plast",
+    image: "/images/mortar/JSW-ENDURO-PLAST.png",
+    category: "mortar",
+    description:
+      "Single component rendering mortar with a ratio of graded inert sand with cement and polymers for applications in civil construction works.",
+    features: ["For Ceillings", "For Walls"],
+    packages: ["40 kg"],
+    benefits: [
+      "Cohesive & workable mortar",
+      "Less water consumption",
+      "Increased Curing efficiency",
+      "Improves bond strength with applied substrate",
+      " Reduce Rebound losses",
+      "Better tensile and flexural strength",
+      "Increase in durability",
+      "Faster rate of application",
+      "Extended application time",
+      "Uniform and smooth finishes",
+      "Reduces wall porosity & voids",
+      "Provides enhanced coverage and saves material",
+    ],
+    relatedProducts: [
+      {
+        title: "JSW TYLFIX TYPE 2",
+        image: "/images/tile-adhesive/JSW TYLFIX TYPE 2.png",
+        link: "/tile-adhesive-grout-cleaner-cc/jsw-tylfix-type-2",
+      },
+    ],
+    specificationsTable: [
+      {
+        property: "Pot life at 30°C min",
+        result: "90-120",
+      },
+      {
+        property: "Compressive strength at 28 days, Mpa",
+        result: "6-8",
+      },
+      {
+        property: "Tensile Strength at 28 dasy, Mpa",
+        result: "Min.1.0",
+      },
+      {
+        property: "Flexural Strength at 28 days, Mpa",
+        result: "Min.1.5",
+      },
+      {
+        property: "Water Absorption%",
+        result: "Max 5",
+      },
+      {
+        property: "Rebound Loss%",
+        result: "Max 2",
+      },
+    ],
+  },
+  {
+    title: "BLOCK GRIP ADHESIVE",
+    subTitle: "EXCELLENT ADHESIVE FOR CURED BLOCKS",
+    slug: "block-grip-adhesive",
+    image: "/images/mortar/JSW-BLOW_GRIP-ADHESIVE.png",
+    category: "mortar",
+    description:
+      "It's a single-component thixotropic thin-bed fixing and jointing block grip adhesive. It contains an aggregate ratio of graded inert sand with cement and polymers for applications as a bond-gripping material in civil construction works. It is recommended for fixing and jointing AAC blocks, ALC blocks, Cement mortar blocks, Cellular concrete blocks, Fly ash bricks and Composite cement blocks.",
+    features: ["For Ceillings", "For Walls"],
+    packages: ["40 kg"],
+    benefits: [
+      "Cohesive & workable mortar",
+      "High tensile & flexural with strong bonding",
+      "Consistent spread for faster rate of application",
+      "Uniform and smooth joint finishes",
+      "Reduced surface porosity & voids",
+      "Better coverage",
+      "Self curing",
+      "Shock and impact resistance",
+      "Resists water seepage",
+    ],
+    relatedProducts: [
+      {
+        title: "JSW TYLFIX TYPE 2",
+        image: "/images/tile-adhesive/JSW TYLFIX TYPE 2.png",
+        link: "/tile-adhesive-grout-cleaner-cc/jsw-tylfix-type-2",
+      },
+    ],
+    specificationsTable: [
+      {
+        property: "Pot life at 30°C min",
+        result: "90-120",
+      },
+      {
+        property: "Compressive strength at 28 days, Mpa",
+        result: "6-8",
+      },
+      {
+        property: "Tensile Strength at 28 dasy, Mpa",
+        result: "Min.1.0",
+      },
+      {
+        property: "Flexural Strength at 28 days, Mpa",
+        result: "Min.1.5",
+      },
+      {
+        property: "Water Absorption%",
+        result: "Max 5",
+      },
+      {
+        property: "Rebound Loss%",
+        result: "Max 2",
+      },
+    ],
+  },
+];
+
+export default data;
--- src/content/tile-adhesive-grout-cleaner-cc.js
+++ src/content/tile-adhesive-grout-cleaner-cc.js
@@ -23,17 +23,17 @@ const data = [
       {
         title: "JSW TYLFIX TYPE 2",
         image: "/images/tile-adhesive/JSW TYLFIX TYPE 2.png",
-        link: "/tile-adhesive-grout-cleaner/jsw-tylfix-type-2",
+        link: "/tile-adhesive-grout-cleaner-cc/jsw-tylfix-type-2",
       },
       {
         title: "JSW TYLFIX TYPE 3",
         image: "/images/tile-adhesive/JSW TYLFIX TYPE 3.png",
-        link: "/tile-adhesive-grout-cleaner/jsw-tylfix-type-2",
+        link: "/tile-adhesive-grout-cleaner-cc/jsw-tylfix-type-2",
       },
       {
         title: "JSW TYLFIX TYPE 4",
         image: "/images/tile-adhesive/JSW TYLFIX TYPE 4.png",
-        link: "/tile-adhesive-grout-cleaner/jsw-tylfix-type-2",
+        link: "/tile-adhesive-grout-cleaner-cc/jsw-tylfix-type-4",
       },
     ],
     specificationsTable: [
@@ -76,6 +76,390 @@ const data = [
       {
         property: "Settings/ trafficable time",
         result: "24 hours",
+      },
+    ],
+  },
+  {
+    title: "JSW TYLFIX TYPE 2",
+    subTitle: "TILE ADISIVE FOR FLOOR & WALL & TILE ON TILE",
+    slug: "jsw-tylfix-type-2",
+    image: "/images/tile-adhesive/JSW TYLFIX TYPE 2.png",
+    category: "tile-adhesive-grout-cleaner",
+    description:
+      "JSW Tylfix Type 2 is a polymer modified cement based powder tile adhesive for fixing All types of ceramic tile, terracotta, pavers, brick vitrified tiles, glass. It is suitable for Flooring and walls, for internal applications & external shaded area, external area, dry & wet area, new construction & renovation, domestic applications, commercial applications.",
+    features: [
+      "Sag Resistant",
+      "Water Resistant",
+      "Better Adhesive Bond Strength",
+    ],
+    packages: ["20 kg"],
+    benefits: [
+      "Easy to use - just add water on site, ready to use.",
+      "Ideal product for tile-on-tile applications on floor.",
+      "Sag resistant formula: provides hassle-free application on vertical walls.",
+      "Water resistant - suitable for wet areas.",
+      "Good adhesive bond strength & bonds to various cement-based substrates.",
+      "Low VOC - for healthy living.",
+    ],
+    relatedProducts: [
+      {
+        title: "JSW TYLFIX TYPE 1",
+        image: "/images/tile-adhesive/JSW TYLFIX  TYPE 1.png",
+        link: "/tile-adhesive-grout-cleaner-cc/jsw-tylfix-type-1",
+      },
+      {
+        title: "JSW TYLFIX TYPE 3",
+        image: "/images/tile-adhesive/JSW TYLFIX TYPE 3.png",
+        link: "/tile-adhesive-grout-cleaner-cc/jsw-tylfix-type-3",
+      },
+      {
+        title: "JSW TYLFIX TYPE 4",
+        image: "/images/tile-adhesive/JSW TYLFIX TYPE 4.png",
+        link: "/tile-adhesive-grout-cleaner-cc/jsw-tylfix-type-4",
+      },
+    ],
+    specificationsTable: [
+      {
+        property: "Tentative mixing ration (Powder: Water)",
+        result: "3.5 : 1 by volume;\n1 : 0.22 by weight",
+      },
+      {
+        property: "Mixed density",
+        result: "1.7 - 1.9 kg/ltr",
+      },
+      {
+        property: "Table flow",
+        result: "180 mm",
+      },
+      {
+        property: "Open time (IS 15477:2019)",
+        result: "Approx. 20 minutes",
+      },
+      {
+        property: "Adjustability\n(IS 15477:2019)",
+        result: "Approx. 20 minutes",
+      },
+      {
+        property: "Pot life",
+        result: "Approx. 120 minutes",
+      },
+      {
+        property: "Tensile adhesion strength\nas per (IS 15477:2019)",
+        result: "≥ 0.5 N/ mm",
+      },
+      {
+        property: "Application temperature\n(substrates & materials)",
+        result: "5°C to 35°C",
+      },
+      {
+        property: "Adhesive thickness",
+        result: "3 - 12 mm",
+      },
+      {
+        property: "Settings/ trafficable time",
+        result: "24 hours",
+      },
+    ],
+  },
+  {
+    title: "JSW TYLFIX TYPE 3",
+    subTitle: "TILE ADISIVE FOR FLOOR & GLASS & STONE TILES",
+    slug: "jsw-tylfix-type-3",
+    image: "/images/tile-adhesive/JSW TYLFIX TYPE 3.png",
+    category: "tile-adhesive-grout-cleaner",
+    description:
+      "JSW Tylfix Type 3 is a polymer modified cement based powder tile adhesive for fixing All types of ceramic tiles, clay tiles, basalt tiles, terracotta, pavers, brick, vitrified tiles, glass mosaic tiles, porcelain tiles and all natural stone tiles. It is suitable for Flooring and walls, for internal applications & external shaded area, external area, dry & wet area, new construction & renovation, domestic applications, commercial applications.",
+    features: [
+      "Sag Resistant",
+      "Water Resistant",
+      "Better Adhesive Bond Strength",
+    ],
+    packages: ["20 kg"],
+    benefits: [
+      "Easy to use - just add water on site, ready to use.",
+      "Ideal product for tiles and stone tiles on exterior wall substrates like plaster or concrete.",
+      "Highly polymer modified provides high adhesion - strength to bond with variety of substrates for fixing a variety of tiles and stones.",
+      "Sag resistant formula: provides hassle-free application on vertical walls.",
+      "Water resistant - suitable for wet areas.",
+      "Sustain heat ageing, making it deal for high temperature locations.",
+      "Low VOC - for healthy living",
+    ],
+    relatedProducts: [
+      {
+        title: "JSW TYLFIX TYPE 1",
+        image: "/images/tile-adhesive/JSW TYLFIX  TYPE 1.png",
+        link: "/tile-adhesive-grout-cleaner-cc/jsw-tylfix-type-1",
+      },
+      {
+        title: "JSW TYLFIX TYPE 2",
+        image: "/images/tile-adhesive/JSW TYLFIX TYPE 2.png",
+        link: "/tile-adhesive-grout-cleaner-cc/jsw-tylfix-type-3",
+      },
+      {
+        title: "JSW TYLFIX TYPE 3",
+        image: "/images/tile-adhesive/JSW TYLFIX TYPE 3.png",
+        link: "/tile-adhesive-grout-cleaner-cc/jsw-tylfix-type-3",
+      },
+    ],
+    specificationsTable: [
+      {
+        property: "Tentative mixing ration (Powder: Water)",
+        result: "3.5 : 1 by volume;\n1 : 0.22 by weight",
+      },
+      {
+        property: "Mixed density",
+        result: "1.8 - 2.0 kg/ltr",
+      },
+      {
+        property: "Table flow",
+        result: "180 mm",
+      },
+      {
+        property: "Open time (IS 15477:2019)",
+        result: "Approx. 32 minutes",
+      },
+      {
+        property: "Adjustability\n(IS 15477:2019)",
+        result: "Approx. 25 minutes",
+      },
+      {
+        property: "Pot life",
+        result: "Approx. 120 minutes",
+      },
+      {
+        property: "Tensile adhesion strength\nas per (IS 15477:2019)",
+        result: "≥ 0.5 N/ mm",
+      },
+      {
+        property: "Application temperature\n(substrates & materials)",
+        result: "5°C to 35°C",
+      },
+      {
+        property: "Adhesive thickness",
+        result: "3 - 12 mm",
+      },
+      {
+        property: "Settings/ trafficable time",
+        result: "24 hours",
+      },
+    ],
+  },
+  {
+    title: "JSW TYLFIX TYPE 4",
+    subTitle: "TILE ADISIVE FOR LARGE GLASS & STONE TILES AND DRY WALL BOARDS",
+    slug: "jsw-tylfix-type-4",
+    image: "/images/tile-adhesive/JSW TYLFIX TYPE 4.png",
+    category: "tile-adhesive-grout-cleaner",
+    description:
+      "JSW Tylfix Type 4 All types of ceramic tiles, clay tiles, basalt tiles,terracotta, pavers, brick, vitrified tiles, glass mosaic tiles, porcelain tiles,natural stone tiles, large format tiles, marble, large thin/slim tiles, low thickness slabs. It is suitable for Flooring an walls, for internal applications & external area, dry & wet area, fountains and swimming pool, new construction & renovation, domestic applications, commercial applications.",
+    features: [
+      "Heat Ageing",
+      "Water Resistant",
+      "Better Adhesive Bond Strength",
+    ],
+    packages: ["20 kg"],
+    benefits: [
+      "Easy to use - just add water on site, ready to use.",
+      "Ideal product for tiles and stone tiles on exterior wall substrates like plaster or concrete.",
+      "Highly polymer modified provides high adhesion - strength to bond with variety of substrates for fixing a variety of tiles and stones.",
+      "Sag resistant formula: provides hassle-free application on vertical walls.",
+      "Sustain heat ageing, making it deal for high temperature locations.",
+    ],
+    relatedProducts: [
+      {
+        title: "JSW TYLFIX TYPE 1",
+        image: "/images/tile-adhesive/JSW TYLFIX  TYPE 1.png",
+        link: "/tile-adhesive-grout-cleaner-cc/jsw-tylfix-type-1",
+      },
+      {
+        title: "JSW TYLFIX TYPE 2",
+        image: "/images/tile-adhesive/JSW TYLFIX TYPE 2.png",
+        link: "/tile-adhesive-grout-cleaner-cc/jsw-tylfix-type-3",
+      },
+      {
+        title: "JSW TYLFIX TYPE 3",
+        image: "/images/tile-adhesive/JSW TYLFIX TYPE 3.png",
+        link: "/tile-adhesive-grout-cleaner-cc/jsw-tylfix-type-3",
+      },
+    ],
+    specificationsTable: [
+      {
+        property: "Tentative mixing ration (Powder: Water)",
+        result: "3.5 : 1 by volume;\n1 : 0.22 by weight",
+      },
+      {
+        property: "Mixed density",
+        result: "1.8 - 2.0 kg/ltr",
+      },
+      {
+        property: "Table flow",
+        result: "Max 190 mm",
+      },
+      {
+        property: "Open time (IS 15477:2019)",
+        result: "Approx. 40 minutes",
+      },
+      {
+        property: "Adjustability\n(IS 15477:2019)",
+        result: "Approx. 30 minutes",
+      },
+      {
+        property: "Pot life",
+        result: "Approx. 120 minutes",
+      },
+      {
+        property: "Tensile adhesion strength\nas per (IS 15477:2019)",
+        result: "≥ 0.5 N/ mm",
+      },
+      {
+        property: "Application temperature\n(substrates & materials)",
+        result: "5°C to 35°C",
+      },
+      {
+        property: "Adhesive thickness",
+        result: "3 - 12 mm",
+      },
+      {
+        property: "Settings/ trafficable time",
+        result: "24 hours",
+      },
+    ],
+  },
+  {
+    title: "JSW TYLFIX GROUT - CEMENTITIOUS",
+    subTitle: "CEMENT-BASED TILE JOIN FILLER GROUT",
+    slug: "jsw-tylfix-grout-cementitious",
+    image: "/images/tile-adhesive/JSW-TYLFIX-GROUT-CEMENTITIOUS.png",
+    category: "tile-adhesive-grout-cleaner",
+    description:
+      "JSW Tylfix Grout is a polymer-modified, cement-based tile joint filler grouting material, which is highly suitable for filling the joints/gap of all types of tiles. It exhibits water- resistant properties, making it an ideal choice for various applications.",
+    features: ["Non Shrink", "Crack Free", "Easily Pouralble"],
+    packages: ["20 kg"],
+    benefits: [
+      "Easy to use & Clean: just mix with water, ready to use",
+      "Polymer modified: crack-free, minimum shrinkage & water resistance",
+      "Suitable for varied tile and stones: All ceramics, vitrified tiles, granites, etc.",
+      "Low VOC: For healthy living.",
+    ],
+    relatedProducts: [
+      {
+        title: "JSW TYLFIX TYPE 1",
+        image: "/images/tile-adhesive/JSW TYLFIX  TYPE 1.png",
+        link: "/tile-adhesive-grout-cleaner-cc/jsw-tylfix-type-1",
+      },
+      {
+        title: "JSW TYLFIX TYPE 2",
+        image: "/images/tile-adhesive/JSW TYLFIX TYPE 2.png",
+        link: "/tile-adhesive-grout-cleaner-cc/jsw-tylfix-type-2",
+      },
+      {
+        title: "JSW TYLFIX CLEANER",
+        image: "/images/tile-adhesive/JSW-TYLFIX-CLEANER.png",
+        link: "/tile-adhesive-grout-cleaner-cc/jsw-tylfix-cleaner",
+      },
+    ],
+    specificationsTable: [
+      {
+        property: "Component Appearence",
+        result: "Cementitious Coloured Powder",
+      },
+      {
+        property: "Tentative Mixing Ratio",
+        result: "1 kg JSW Tylfix Grout: 0.33 - 0.40 kg water",
+      },
+      {
+        property: "(JSW Tylfix Grout: Water)",
+        result: "(2.5-3:1 by weight)",
+      },
+      {
+        property: "Mixed Density",
+        result: "1.40 - 1.80 kg/ltr",
+      },
+      {
+        property: "Pot life @ 27+20°C Min.",
+        result: "45 minutes.",
+      },
+      {
+        property: "Joint Width",
+        result: "Upto 3 mm",
+      },
+      {
+        property: "Application temperature",
+        result: "10°C to 35°C",
+      },
+      {
+        property: "Light Foot Traffic",
+        result: "24 hours",
+      },
+      {
+        property: "Regular Traffic",
+        result: "7 Days",
+      },
+      {
+        property: "Final Curing Time",
+        result: "28 Days",
+      },
+    ],
+  },
+  {
+    title: "JSW TYLFIX CLEANER",
+    subTitle: "STRONG PERFORMANCE CLEANER",
+    slug: "jsw-tylfix-cleaner",
+    image: "/images/tile-adhesive/JSW-TYLFIX-GROUT-CEMENTITIOUS.png",
+    category: "tile-adhesive-grout-cleaner",
+    description:
+      "Tylfix Cleaner is a strong performance rapid active cleaner and degreasing solution for use on tiles, bathtubs, wash basin, privy basin and concrete etc. It is used to clean whitish spot/ layer as normally observed in the bathroom under plastic/ metal buckets (normal detergent is not helpful) with ease",
+    features: [
+      "Easy Cleaning",
+      "No Residual Marks",
+      "Cleans all types of Tiles",
+    ],
+    packages: ["20 kg"],
+    benefits: [
+      "Easy to use: Just mix with water & clean the surface.",
+      "Removes difficult stains easily: Fast & effective.",
+      "Degreases the tile surface easily for soaps, scum, efflorescence, mineral deposit. I",
+      "Cleans all types of tile like ceramic, porcelain & vitrified tile and grout.",
+      "Protects the color & shine of tile.",
+      "Does not damage joints between ceramic tiles, even those grouted with colored products.",
+    ],
+    relatedProducts: [
+      {
+        title: "JSW TYLFIX TYPE 1",
+        image: "/images/tile-adhesive/JSW TYLFIX  TYPE 1.png",
+        link: "/tile-adhesive-grout-cleaner-cc/jsw-tylfix-type-1",
+      },
+      {
+        title: "JSW TYLFIX TYPE 2",
+        image: "/images/tile-adhesive/JSW TYLFIX TYPE 2.png",
+        link: "/tile-adhesive-grout-cleaner-cc/jsw-tylfix-type-2",
+      },
+      {
+        title: "JSW TYLFIX GROUT - CEMENTITIOUS",
+        image: "/images/tile-adhesive/JSW-TYLFIX-GROUT-CEMENTITIOUS.png",
+        link: "/tile-adhesive-grout-cleaner-cc/jsw-tylfix-grout-cementitious",
+      },
+    ],
+    specificationsTable: [
+      {
+        property: "Appearence",
+        result: "Reddish color liquid",
+      },
+      {
+        property: "Density",
+        result: "1.02 + 0.02 kg/ltr",
+      },
+      {
+        property: "Viscosity",
+        result: "Equal to water",
+      },
+      {
+        property: "Soaking time",
+        result: "5 minutes",
+      },
+      {
+        property: "Dilution with water",
+        result: "Depend upon substrate condition & stain severity",
       },
     ],
   },
